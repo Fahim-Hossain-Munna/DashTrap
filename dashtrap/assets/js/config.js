b4178a9c42f6edc29f01ac4f0cbb2e2313e96354
Fahim Hossain Munna <87969487+Fahim-Hossain-Munna@users.noreply.github.com>
2024-03-22 09:51:44 +0600

    hello how are you?

---
+++ dashtrap/assets/js/config.js
@@ -0,0 +1,1 @@
+class Config{adjustLayout(){const i=document.getElementsByTagName("html")[0];window.innerWidth<=1200&&i.setAttribute("data-sidebar-size","full")}initWindowSize(){var t=this;window.addEventListener("resize",function(i){t.adjustLayout()})}init(){this.adjustLayout(),this.initWindowSize()}}(new Config).init();
